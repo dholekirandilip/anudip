c79b3bda2ba8717f29d44d89033d46689a0a882b
dholekirandilip <kirandhole59@gmail.com>
2025-06-09 12:30:36 +0530

    Add files via upload

---
+++ Continue_program.java
@@ -0,0 +1,13 @@
+ public class Continue_program{
+  public static void main(String[] args) {
+    int n=0;
+      for(int i=1;i<=100;i++){
+         n=i+n;
+         if(i==3)
+         continue;
+        System.out.println(n);
+      }
+  }
+ }
+    
+
