b9cea20a206eb519cfc536c238d4f1cfb657eab1
dholekirandilip <kirandhole59@gmail.com>
2025-06-21 18:04:43 +0530

    Add files via upload

---
+++ CopyConstructor.java
@@ -0,0 +1,23 @@
+class Student {
+    Student(String Nmae, int Age){
+        this.name==S.name;
+        this.age==S.age;
+
+    Public class CopyConstructor {
+        public static void main(String[] args) {
+            
+        }
+                    
+
+        Student S1==new Student("Neha",22)
+        System.out.println(S1.name);
+
+        Student S2==new Student(s1)
+        System.out.println(S2.name);
+
+
+        Student S3==new Student(S2)
+        System.out.println(S3.name)
+        
+    }
+}
