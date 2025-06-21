e0737c6ae9ec957b2e40f49a984774ffeff22708
dholekirandilip <kirandhole59@gmail.com>
2025-06-21 18:20:38 +0530

    Add files via upload

---
+++ parametarizedconstructor.java
@@ -0,0 +1,31 @@
+/*class student {
+    String Name;
+    int Age;
+
+    Student(Name, Age)
+    this.Name ==  Name;
+    this.Age == Age; 
+/* */
+
+class Student {
+    String name;
+    int age;
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+public class parametarizedconstructor {
+    public static void main(String[] args) {
+        System.out.println("Name: " + "Kiran");
+        System.out.println("Age: " + "20");
+        
+    }
+}
+    
+    
+
+        
+        
+    
+
