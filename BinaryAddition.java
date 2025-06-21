fc52111381fc269fd990b7b1110a2c15a1cbb13a
dholekirandilip <kirandhole59@gmail.com>
2025-06-21 18:02:14 +0530

    Add files via upload

---
+++ BinaryAddition.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first binary number: ");
+        String binary1 = scanner.nextLine();
+
+        System.out.print("Enter second binary number: ");
+        String binary2 = scanner.nextLine();
+
+        int num1 = Integer.parseInt(binary1, 2);
+        int num2 = Integer.parseInt(binary2, 2);
+
+        int sum = num1 + num2;
+
+        String binarySum = Integer.toBinaryString(sum);
+
+
+        System.out.println("Sum of two binary numbers: " + binarySum);
+    }
+}
