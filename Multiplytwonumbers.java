5845ff818e7a55a473822aa3b0c3e4ca977bd171
dholekirandilip <kirandhole59@gmail.com>
2025-06-21 18:15:53 +0530

    Add files via upload

---
+++ Multiplytwonumbers.java
@@ -0,0 +1,11 @@
+public class Multiplytwonumbers {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+
+        int result=num1 * num2;
+
+        System.out.println("The multiplication of  " + num1 + "and" + num2 + "is:" + result);
+    }
+    
+}
