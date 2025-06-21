0d6e74657ddef8b8acd29a5d6e43f5782776f70c
dholekirandilip <kirandhole59@gmail.com>
2025-06-21 18:24:41 +0530

    Add files via upload

---
+++ SimpleArray.java
@@ -0,0 +1,21 @@
+/*public class 1D_Array {
+    public static void main(String[] args) {
+        int number [] = new int [5];
+        int matrix [][] = int new [4][5];
+        System.out.println(number[2]);
+    }
+    
+}*/
+
+public class SimpleArray {
+    public static void main(String[] args) {
+        // Declare and initialize a 1D array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print array elements
+        System.out.println("Elements in the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
